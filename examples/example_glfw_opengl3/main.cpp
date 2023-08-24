373eea9ef4bde4574989598ef57d8479a5bca22a
lnd3 <linus.cumselius@gmail.com>
2023-08-24 03:34:46 +0200

    Fix imgui example.

--- examples/example_glfw_opengl3/main.cpp
+++ examples/example_glfw_opengl3/main.cpp
@@ -3,9 +3,9 @@
 // If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
 // Read online: https://github.com/ocornut/imgui/tree/master/docs
 
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 #include <stdio.h>
 #define GL_SILENCE_DEPRECATION
 #if defined(IMGUI_IMPL_OPENGL_ES2)
